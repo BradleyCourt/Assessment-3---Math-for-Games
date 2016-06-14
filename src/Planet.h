3d2a1f8fcebee7ec9098d4e5d01b5a3e418f93c0
Bradley Court <bradley.court@ad.aie.edu>
2016-06-15 08:43:35 +0930

    added incldues

--- src/Planet.h
+++ src/Planet.h
@@ -1,16 +1,19 @@
 #pragma once
+#include "BaseApplication.h"
+#include "Vector.h"
 #include "Matrix3.h"
 class Texture;
 class SpriteBatch;
 class Planet
 {
-public:
+private:
 	Planet* m_Parent;
 	float m_rotationSpeed, m_rotationAngle;
 	Matrix3 m_localTrans, m_globalTrans, m_scaleTrans, m_rotationTrans, m_translationTrans;
 	Texture* m_tex;
 	SpriteBatch* m_sBatch;
-	Planet();
+public:
+	Planet(Texture* tex, Planet* parent, float rotateSpeed, float rotateAngle);
 	~Planet();
 	void update(float T);
 };
